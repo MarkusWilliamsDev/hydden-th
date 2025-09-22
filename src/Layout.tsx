e891ed498025747761772e790ad06286cbf71c93
MarkusWilliams <markuswilliamsdev@gmail.com>
2025-09-22 19:03:20 -0400

    Fix sidenav hiding content beneath

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -3,14 +3,12 @@ import Sidebar from "./components/Sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
-		<div className="drawer">
-			<div className="drawer-content flex flex-col">
-				<Navbar />
-				<div className="flex-1 lg:ml-64">
-					<main className="p-4">{children}</main>
-				</div>
-			</div>
+		<div className="flex">
 			<Sidebar />
+			<div className="flex-1 flex flex-col">
+				<Navbar />
+				<main className="flex-1 p-4">{children}</main>
+			</div>
 		</div>
 	);
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,6 @@
 export default function Sidebar() {
 	return (
-		<aside className="w-64 min-h-screen bg-base-200 hidden lg:block fixed left-0 top-0 z-10">
+		<aside className="w-64 min-h-screen bg-base-200 hidden lg:block">
 			<div className="p-4">
 				<h2 className="text-xl font-bold mb-6">Menu</h2>
 				<ul className="menu">
