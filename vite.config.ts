fdf72e9f4282ef60266102a21009697d66d77cde
MarkusWilliams <markuswilliamsdev@gmail.com>
2025-09-25 13:08:24 -0400

    setup react testing library

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
@@ -5,5 +7,10 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
+	test: {
+		environment: "jsdom",
+		globals: true,
+		setupFiles: "./src/tests/setup.js",
+	},
 });
 
